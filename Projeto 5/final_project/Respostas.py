ed8e746d1837827ea15c2cfa311f0341d14e6744
William Moura Mesquita <william.m.mesquita@gmail.com>
2017-12-17 13:34:06 +0000

    Projeto 5 - Primeira Versão

---
+++ Projeto 5/final_project/Respostas.py
@@ -0,0 +1,99 @@
+import sys
+import pickle
+sys.path.append("../tools/")
+
+from feature_format import featureFormat, targetFeatureSplit
+from tester import dump_classifier_and_data
+
+def testNaN(Numero):
+    if Numero=="NaN":
+        return True
+    else:
+        return False
+
+def find_maximo(variavel,data):
+    maximo = 0
+    for key in data:
+        if(testNaN(data[key][variavel])!=True):
+            if(maximo < data[key][variavel]):
+                maximo=data[key][variavel]
+    return maximo
+
+def calc_frac(my_dataset, Person, Divisor, Variavel):
+    if(testNaN(my_dataset[Person][Variavel])):
+        return "NaN"
+    else:
+        return my_dataset[Person][Variavel]*1.0/Divisor
+
+
+def calc_mult(my_dataset,Person):
+        if(testNaN(my_dataset[Person]['frac_from_this_person_to_poi']) or testNaN(my_dataset[Person]['frac_from_poi_to_this_person'])):
+            return "NaN"
+        else:
+            return my_dataset[Person]['frac_from_this_person_to_poi']*my_dataset[Person]['frac_from_poi_to_this_person']
+
+
+### Task 1: Select what features you'll use.
+### features_list is a list of strings, each of which is a feature name.
+### The first feature must be "poi".
+features_list = ['poi','salary','bonus','total_payments','expenses', 'total_stock_value','from_poi_to_this_person','from_this_person_to_poi', 'shared_receipt_with_poi','from_messages','to_messages'] # You will need to use more features
+
+### Load the dictionary containing the dataset
+with open("final_project_dataset.pkl", "r") as data_file:
+    data_dict = pickle.load(data_file)
+
+
+
+### Task 2: Remove outliers
+
+del data_dict['TOTAL']
+
+### Task 3: Create new feature(s)
+features_list.append('frac_salary')
+features_list.append('frac_bonus')
+features_list.append('frac_total_payments')
+features_list.append('frac_expenses')
+features_list.append('frac_total_stock_value')
+features_list.append('frac_from_poi_to_this_person')
+features_list.append('frac_from_this_person_to_poi')
+features_list.append('frac_shared_receipt_with_poi')
+features_list.append('multiply_emails')
+
+del features_list[1:11]
+### Store to my_dataset for easy export below.
+my_dataset = data_dict
+
+MaxSalary=find_maximo("salary",my_dataset)
+MaxBonus=find_maximo("bonus",my_dataset)
+MaxPayments=find_maximo("total_payments",my_dataset)
+MaxExpenses=find_maximo("expenses",my_dataset)
+MaxStock=find_maximo("total_stock_value",my_dataset)
+
+
+
+for person in my_dataset:
+        my_dataset[person]['frac_salary'] = calc_frac(my_dataset,person,MaxSalary,"salary")
+        my_dataset[person]['frac_bonus'] = calc_frac(my_dataset,person,MaxBonus,"bonus")
+        my_dataset[person]['frac_total_payments'] = calc_frac(my_dataset,person,MaxPayments,"total_payments")
+        my_dataset[person]['frac_expenses'] = calc_frac(my_dataset,person,MaxExpenses,"expenses")
+        my_dataset[person]['frac_total_stock_value'] = calc_frac(my_dataset,person,MaxStock,"total_stock_value")
+        my_dataset[person]['frac_from_poi_to_this_person'] = calc_frac(my_dataset,person,my_dataset[person]['to_messages'],"from_poi_to_this_person")
+        my_dataset[person]['frac_from_this_person_to_poi'] = calc_frac(my_dataset,person,my_dataset[person]['from_messages'],"from_this_person_to_poi")
+        my_dataset[person]['frac_shared_receipt_with_poi'] = calc_frac(my_dataset,person,my_dataset[person]['to_messages'],"shared_receipt_with_poi")
+        my_dataset[person]['multiply_emails'] = calc_mult(my_dataset,person)
+
+soma = 0
+for i in data_dict:
+    soma += my_dataset[i]['poi']
+SomaTotal = []
+SomaAux = 0
+for feature in features_list:
+    for pessoa in my_dataset:
+        if data_dict[pessoa][feature]=='NaN':
+            SomaAux+=1
+    SomaTotal.append(SomaAux)
+    SomaAux=0
+#Questao 1
+print len(data_dict),soma,len(features_list)-1,SomaTotal
+#Questao 2
+print features_list
